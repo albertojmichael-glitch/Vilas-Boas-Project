5d0182c01937d986afa5f659f90c6c20bd1a6d2e
alberto <albertojmichael@gmail.com>
2026-07-20 11:41:00 -0300

    sd

--- minigames.py
+++ minigames.py
@@ -1,7 +1,7 @@
 import random
 from ui import DOS_VERDE, DOS_BRANCO, DOS_AMARELO, DOS_VERMELHO, RESET, limpar_tela, pausar, digitar
 from data import ARTE_INDIO
-from data import DOS_VERMELHO, DOS_AMARELO, DOS_VERDE, RESET, CAVEIRA_ASCII
+from engine import DOS_VERMELHO, DOS_AMARELO, DOS_VERDE, RESET, CAVEIRA_ASCII
 
 CAVEIRA_MORTE = r'''
                      .ed"""" """$$$$be.
